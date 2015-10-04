15528f2f5af8e67a330001e86dd0703fdee39cb3
BillMills <mills.wj@gmail.com>
2015-10-04 15:44:36 -0700

    coming up on early demo

--- js/dataStore.js
+++ js/dataStore.js
@@ -11,5 +11,64 @@ window.dataStore = {}
 dataStore.melbourne = {
     'location': 'University of Melbourne',
     'dates': 'February 1-3, 2016',
-    'map': 'sites/melbourne.geojson'
+    'map': 'sites/melbourne.geojson',
+    'days': [
+        {
+            'day': 'February 1',
+            'sessions': [
+                'Session One',
+                'Session Two',
+                'Session Three'
+            ]
+        },
+        {
+            'day': 'February 2',
+            'sessions': [
+                'Session One',
+                'Session Two',
+                'Session Three'
+            ]
+        },
+        {
+            'day': 'February 3',
+            'sessions': [
+                'Session One',
+                'Session Two',
+                'Session Three'
+            ]
+        }
+    ],
+    'speakers': [
+        {
+            'img': 'img/speaker.jpg',
+            'name': 'Dr. Pika',
+            'blurb': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+            'img': 'img/speaker.jpg',
+            'name': 'Dr. Pika',
+            'blurb': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+            'img': 'img/speaker.jpg',
+            'name': 'Dr. Pika',
+            'blurb': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+            'img': 'img/speaker.jpg',
+            'name': 'Dr. Pika',
+            'blurb': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+            'img': 'img/speaker.jpg',
+            'name': 'Dr. Pika',
+            'blurb': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+            'img': 'img/speaker.jpg',
+            'name': 'Dr. Pika',
+            'blurb': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+    ]
+
 }
