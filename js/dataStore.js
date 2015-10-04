e91c42d33a37f39c1fd60dc84a85446d65ecc067
BillMills <mills.wj@gmail.com>
2015-10-04 14:43:30 -0700

    geojson links

--- js/dataStore.js
+++ js/dataStore.js
@@ -2,6 +2,10 @@ function fetchData(data){
     return window.dataStore[data.site]
 }
 
+function injectMap(){
+    document.getElementById('maptarget').innerHTML = layout.partials.map
+}
+
 window.dataStore = {}
 
 dataStore.melbourne = {
